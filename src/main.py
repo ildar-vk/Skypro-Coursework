a8c2e7f1c811f7b49cbc8f43be80ba46d0552bb5
ildar-vk <kalimov.ildar.v@gmail.com>
2025-10-20 17:22:53 +0500

    Продолжение работы над task1.в task2 загружен файл операций и исправлен путь в файлу.Установлена недостающая библиотека в poetry

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from utils import process_bank_file
 
 def main():
     try:
-        file_path = "data/operations.xlsx"
+        file_path = "/home/oem/PycharmProjects/Courcework/data/operations.xlsx"
         df = process_bank_file(file_path)
         print(f"Готово! Загружено {len(df)} транзакций")
     except Exception as e:
-        print(f"{__name__} Ошибка:{e}")
+        print(f"{__name__} Ошибка: {e}")
 
 if __name__ == "__main__":
     main()
